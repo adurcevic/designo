478a9cb908ff6da2a7a6dacaa3d2e2a51099ed08
Antonio Đurčević <antonio.durcevic@gmail.com>
2023-01-10 10:22:19 +0100

    added error page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import ErrorPage from '../components/ErrorPage/ErrorPage';
+import GenericPageMeta from '../components/PageMeta/GenericPageMeta';
+
+const Custom404 = () => {
+  return (
+    <>
+      <GenericPageMeta title={'404: Not found'} />
+      <ErrorPage title={'404'} description={'This page could not be found'} />
+    </>
+  );
+};
+
+export default Custom404;
--- pages/locations.tsx
+++ pages/locations.tsx
@@ -37,9 +37,8 @@ export default locations;
 export const getStaticProps: GetStaticProps = () => {
   const props = {
     meta: {
-      title: 'About us',
-      description:
-        'Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.',
+      title: 'Locations',
+      description: 'Find out about our office locations.',
     },
     locations: [
       {
