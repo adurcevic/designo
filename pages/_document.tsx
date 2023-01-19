3da0c3d563f50d93aadd30493389ecad2a227a05
Antonio Đurčević <antonio.durcevic@gmail.com>
2023-01-19 17:22:30 +0100

    added confirmation for google search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="google-site-verification"
+          content="73cOrqj67zGNLca-LK5pQQoOU1HmrdN1h12B_5X1d4k"
+        />
         <link rel="icon" href="/assets/favicon-32x32.png" />
         {runtimeEnv !== 'production' && (
           <meta name="robots" content="noindex" />
